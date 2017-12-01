325e5a1e56898a0a3036f8614cb1afeba3ab4376
chaospredictor <kuzi81@gmail.com>
2017-12-02 00:42:35 +0200

    findAdvanced test added to Splay

--- wet1/unitTestsSplay.cpp
+++ wet1/unitTestsSplay.cpp
@@ -60,12 +60,52 @@ void testInsertAdvanced(){
     
 }
 
+void testFindAdvanced() {
+    SplayTree<int> tree;
+    
+    int* a = new int(35);
+    int* b = new int(9);
+    int* c = new int(2);
+    int* d = new int(18);
+    int* e = new int(44);
+    int* f = new int(73);
+    int* g = new int(69);
+    int* h = new int(39);
+    int* i = new int(43);
+    int* j = new int(96);
+    int* k = new int(99);
+    int* l = new int(100);
+
+    tree.insert(a);
+    tree.insert(b);
+    tree.insert(c);
+    tree.insert(d);
+    tree.insert(e);
+    tree.insert(f);
+    tree.insert(g);
+    tree.insert(h);
+    tree.insert(i);
+    tree.insert(j);
+    tree.insert(k);
+    tree.insert(l);
+    
+    ASSERT_TRUE(tree.find(35));
+    ASSERT_TRUE(tree.find(39));
+    ASSERT_TRUE(tree.find(100));
+    ASSERT_TRUE(tree.find(99));
+    ASSERT_TRUE(tree.find(69));
+    ASSERT_FALSE(tree.find(131));
+    ASSERT_FALSE(tree.find(41));
+    ASSERT_FALSE(tree.find(60));
+    //    tree.inOrder();
+}
+
 
 int main() {
 	RUN_TEST(testSplay);
     RUN_TEST(testBasicTreeInsert);
     RUN_TEST(testFind);
-    //    RUN_TEST(testFindAdvanced);
+    RUN_TEST(testFindAdvanced);
     RUN_TEST(testInsertAdvanced);
 	return 0;
 }
