98e378abdb0be61b019b4e7d79c6662eccf445c6
chaospredictor <kuzi81@gmail.com>
2017-12-01 14:03:17 +0200

    tests from BinatyTree copied to SplayTree

--- wet1/unitTestsSplay.cpp
+++ wet1/unitTestsSplay.cpp
@@ -23,8 +23,50 @@ void testSplay() {
 }
 
 
+void testBasicTreeInsert() {
+    int* a = new int(1);
+    int* b = new int(2);
+    int* c = new int(3);
+    
+    SplayTree<int> tree;
+    tree.insert(b);
+    tree.insert(a);
+    tree.insert(c);
+    //    tree.inOrder()
+}
+
+void testFind() {
+    SplayTree<int> tree;
+    int* a = new int(1);
+    int* b = new int(2);
+    int* c = new int(3);
+    tree.insert(b);
+    tree.insert(a);
+    tree.insert(c);
+    
+    ASSERT_TRUE(tree.find(3));
+    ASSERT_TRUE(tree.find(1));
+    ASSERT_FALSE(tree.find(31));
+    
+    
+}
+
+void testInsertAdvanced(){
+    SplayTree<int> tree;
+    int* a = new int(1);
+    ASSERT_TRUE(tree.insert(a));
+    ASSERT_FALSE(tree.insert(a));
+    //    tree.inOrder();
+    
+}
+
+
 int main() {
 	RUN_TEST(testSplay);
+    RUN_TEST(testBasicTreeInsert);
+    RUN_TEST(testFind);
+    //    RUN_TEST(testFindAdvanced);
+    RUN_TEST(testInsertAdvanced);
 	return 0;
 }
 
